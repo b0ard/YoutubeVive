36f26134dd9adb1a8513941db694a2524f055101
Sean Lee <slee@alumni.ubc.ca>
2016-03-13 22:48:22 -0700

    Picking up objects in VR with wand by childing object to wand
    Tried:
    -Making it kinematic
    -Turning off gravity
    
    Neither really works very well

--- Assets/WandController.cs
+++ Assets/WandController.cs
@@ -3,18 +3,12 @@ using System.Collections;
 
 public class WandController : MonoBehaviour {
     private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
-    public bool gripButtonDown = false;
-    public bool gripButtonUp = false;
-    public bool gripButtonPressed = false;
-
-
     private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
-    public bool triggerButtonDown = false;
-    public bool triggerButtonUp = false;
-    public bool triggerButtonPressed = false;
 
     private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
     private SteamVR_TrackedObject trackedObj;
+
+    private GameObject pickup;
 
 	// Use this for initialization
 	void Start () {
@@ -28,25 +22,21 @@ public class WandController : MonoBehaviour {
             return;
         }
 
-        gripButtonDown = controller.GetPressDown(gripButton);
-        gripButtonUp = controller.GetPressUp(gripButton);
-        gripButtonPressed = controller.GetPress(gripButton);
-
-        triggerButtonDown = controller.GetPressDown(triggerButton);
-        triggerButtonUp = controller.GetPressUp(triggerButton);
-        triggerButtonPressed = controller.GetPress(triggerButton);
-
-        if (gripButtonDown) {
-            Debug.Log("Grip Button was just pressed");
+        if (controller.GetPressDown(gripButton) && pickup != null) {
+            pickup.transform.parent = this.transform;
+            pickup.GetComponent<Rigidbody>().useGravity = false;
         }
-        if (gripButtonUp) {
-            Debug.Log("Grip Button was just unpressed");
-        }
-        if (triggerButtonDown) {
-            Debug.Log("Trigger Button was just pressed");
-        }
-        if (triggerButtonUp) {
-            Debug.Log("Trigger Button was just unpressed");
+        if (controller.GetPressUp(gripButton) && pickup != null) {
+            pickup.transform.parent = null;
+            pickup.GetComponent<Rigidbody>().useGravity = true;
         }
 	}
+
+    private void OnTriggerEnter(Collider collider) {
+        pickup = collider.gameObject;
+    }
+
+    private void OnTriggerExit(Collider collider) {
+        pickup = null;
+    }
 }
